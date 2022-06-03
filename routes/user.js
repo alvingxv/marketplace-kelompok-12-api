711b4799d51fbf78beb7ec8e66be0d6e6fe3ed47
Alvian Ghifari <alvian2002286@gmail.com>
2022-06-03 23:43:31 +0700

    update routes my order

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ userRouter.post('/addproduct', user_controller.add_product);
 userRouter.post('/updateproduct', user_controller.update_product);
 userRouter.get('/getallproduct', user_controller.get_all_product);
 userRouter.post('/buy', user_controller.buy_product);
-userRouter.get('/myorder', user_controller.my_order);
+userRouter.gept('/myorder', user_controller.my_order);
 userRouter.post('/pay', user_controller.pay_orders);
 userRouter.post('/sellerconfirm', user_controller.seller_confirm);
 userRouter.post('/buyerconfirm', user_controller.buyer_confirm);
